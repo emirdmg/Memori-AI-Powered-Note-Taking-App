eee5ec48a4cb0f7604fb678ffe3424c8983fcfbc
Emirhan Sen <emirhansen35@gmail.com>
2025-09-10 20:22:48 +0300

    first commit

---
+++ lib/Screens/main_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          'memori',
+          style: TextStyle(
+              fontFamily: 'PoiretOne',
+              fontSize: 24
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60),
+                color: Colors.grey.shade200
+              ),
+              child: Center(
+                  child: Text(
+                      '😴',
+                    style: TextStyle(
+                      fontSize: 30
+                    ),
+                  )
+              ),
+            ),
+            Text(
+                'There is no content yet.',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey.shade600
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:memori_ai/Screens/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: MainPage(),
+    );
+  }
+}
+
+
